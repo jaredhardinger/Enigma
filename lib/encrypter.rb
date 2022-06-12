2617f4b4c700090e7dff0ad0663c30775ee2ed06
Jared Hardinger <jaredhardinger@gmail.com>
2022-06-12 14:11:41 -0400

    Add message to nums method

--- lib/encrypter.rb
+++ lib/encrypter.rb
@@ -1,8 +1,16 @@
 class Encrypter
+
   def initialize(message, key, date)
     @message = message
     @key = key
     @date = date
+    @char_map = ('a'..'z').to_a << ' '
+    @char_hash = {}
+    n = 0
+    until n == 27
+      @char_hash[@char_map[n]] = n+1
+      n+=1
+    end
   end
 #maybe throw all class methods into 'class << self and def each below'
   def encrypt
@@ -20,4 +28,10 @@ class Encrypter
     offsets = square_chars.last(4).map(&:to_i)
   end
 
+  def message_to_nums
+    char_array = @message.chars.to_a
+    char_array.map do |char|
+      @char_map.include?(char) ? @char_hash[char] : char
+    end
+  end
 end
