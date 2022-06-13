bff52ac2f1bd3a224fd289fd60fe90e3fba3677a
Jared Hardinger <jaredhardinger@gmail.com>
2022-06-13 12:29:08 -0400

    Edit tests to expect non-specific hashes

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -1,5 +1,4 @@
 require 'rspec'
-require 'date'
 require './lib/enigma'
 
 RSpec.describe Enigma do
@@ -11,25 +10,25 @@ RSpec.describe Enigma do
 		expect(@enigma).to be_a(Enigma)
 	end
 
-	xit 'can encrypt a message with a key and date' do
+	it 'can encrypt a message with a key and date' do
 		expect(@enigma.encrypt("hello world", "02715", "040895")).to eq({encryption: "keder ohulw", key: "02715", date: "040895"})
-  end
+	end
 
-  xit 'can decrypt a message with a key and date' do
+	it 'can decrypt a message with a key and date' do
 		expect(@enigma.decrypt("keder ohulw", "02715", "040895")).to eq({decryption: "hello world", key: "02715", date: "040895"})
-  end
+	end
 
-  xit 'can encrypt a message with a key (uses todays date)' do
-    encrypted = @enigma.encrypt("hello world", "02715")
-		expect(encrypted).to eq({encryption: "*****", key: "*****", date: "*****"})
-  end
+	it 'can encrypt a message with a key (uses todays date)' do
+		encrypted = @enigma.encrypt("hello world", "02715")
+		expect(encrypted).to be_a(Hash)
+	end
 
-  xit 'can decrypt a message with a key (uses todays date)' do
-    decrypted = @enigma.decrypt("**********", "02715")
-		expect(decrypted).to eq({encryption: "*****", key:"*****" , date:"*****" })
-  end
+	it 'can decrypt a message with a key (uses todays date)' do
+		decrypted = @enigma.decrypt("pmjdwhugztb", "02715")
+		expect(decrypted).to be_a(Hash)
+	end
 
-  xit 'can encrypt a message (generates random key and uses todays date)' do
-		expect(@enigma.encrypt("hello world")).to eq({encryption: "*****", key:"*****" , date:"*****" })
-  end
+	it 'can encrypt a message (generates random key and uses todays date)' do
+		expect(@enigma.encrypt("hello world")).to be_a(Hash)
+	end
 end
