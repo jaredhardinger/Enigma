213523c2bbdb85203892023da9611aaf23b2f179
Jared Hardinger <jaredhardinger@gmail.com>
2022-06-12 21:26:32 -0400

    Add final shift method

--- spec/encrypter_spec.rb
+++ spec/encrypter_spec.rb
@@ -18,6 +18,10 @@ RSpec.describe Encrypter do
 		expect(@encryption.offset).to eq([1, 0, 2, 5])
 	end
 
+	it 'can calculate the final shifts' do
+		expect(@encryption.final_shift).to eq([3, 27, 73, 20])
+	end
+
 	it 'can return each characters corresponding number in sequence' do
 		expect(@encryption.message_to_nums).to eq([8, 5, 12, 12, 15, 27, 23, 15, 18, 12, 4])
 	end
