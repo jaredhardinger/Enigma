6ea1f7fe6d1d02e941dd8ed04e5cfa27a882be8a
Jared Hardinger <jaredhardinger@gmail.com>
2022-06-13 12:30:00 -0400

    Add requires and edit strftime arg

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,15 +1,16 @@
+require 'date'
+require 'encrypter'
+require 'decrypter'
+
 class Enigma
-  TODAY = Date.today.strftime(%d%m%y)
+  TODAY = Date.today.strftime('%d%m%y')
   RANDOM = rand(10**5).to_s.rjust(5,'0')
 
-  def initialize
-  end
-
   def encrypt(message, key = RANDOM, date = TODAY)
-    Encrypt.new(message.downcase, key, date).encrypt
+    Encrypter.new(message.downcase, key, date).encrypt
   end
 
-  def decrypt(message, key, date = TODAY)
-    Decrypt.new(message.downcase, key, date).encrypt
+  def decrypt(message, key = RANDOM, date = TODAY)
+    Decrypter.new(message.downcase, key, date).decrypt
   end
 end
