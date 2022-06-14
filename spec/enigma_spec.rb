436378aa1327a505303fa2297e7a462147ae91aa
Jared Hardinger <jaredhardinger@gmail.com>
2022-06-14 19:05:47 -0400

    Add line breaks for lengthy line

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -11,11 +11,13 @@ RSpec.describe Enigma do
 	end
 
 	it 'can encrypt a message with a key and date' do
-		expect(@enigma.encrypt("hello world", "02715", "040895")).to eq({encryption: "keder ohulw", key: "02715", date: "040895"})
+		expect(@enigma.encrypt("hello world", "02715", "040895")).to \
+		eq({encryption: "keder ohulw", key: "02715", date: "040895"})
 	end
 
 	it 'can decrypt a message with a key and date' do
-		expect(@enigma.decrypt("keder ohulw", "02715", "040895")).to eq({decryption: "hello world", key: "02715", date: "040895"})
+		expect(@enigma.decrypt("keder ohulw", "02715", "040895")).to \
+		eq({decryption: "hello world", key: "02715", date: "040895"})
 	end
 
 	it 'can encrypt a message with a key (uses todays date)' do
